3e03964414841222717d61e3b12f9c92c639d917
Fawaz Al-Mutairi <q8fawazo@hotmail.com>
2018-11-06 20:13:53 +0300

    Update types.js

--- store/actions/types.js
+++ store/actions/types.js
@@ -1,7 +1,6 @@
 // Coffee Types
 export const GET_COFFEESHOPS = "GET_COFFEESHOPS";
 export const COFFEESHOPS_LOADING = "COFFEESHOPS_LOADING";
-export const GET_COFFEESHOP_BY_ID = "GET_COFFEESHOP_BY_ID";
 
 // Auth Types
 export const SET_CURRENT_USER = "SET_CURRENT_USER";
